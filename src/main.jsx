024603bf39b8c2a9866e25c77a57a0eb67fab797
diegoalphanogueira-commits <diegoalphanogueira@gmail.com>
2026-09-25 17:14:37 -0300

    feat: switch showcase to professional Korax V3

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import UltraShowcase from './UltraShowcase.jsx'
-import './ultra-showcase.css'
+import UltraShowcasePro from './UltraShowcasePro.jsx'
+import './ultra-showcase-pro.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UltraShowcase />
+    <UltraShowcasePro />
   </React.StrictMode>,
 )
