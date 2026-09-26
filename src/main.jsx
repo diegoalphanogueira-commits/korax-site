f59a0ec8bd65bc6f4d4bd51fb5e5bc7e812a4dab
diegoalphanogueira-commits <diegoalphanogueira@gmail.com>
2026-09-26 01:12:42 -0300

    fix: apply training copy and desktop headline adjustments

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import KoraxLandingV6 from './KoraxLandingV6.jsx'
+import TrainingCopyPatch from './TrainingCopyPatch.jsx'
 import './korax-landing-v5.css'
 import './v5plus-authority-footer.css'
 import './korax-landing-v6.css'
@@ -12,5 +13,6 @@ import './training-desktop-fix.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <KoraxLandingV6 />
+    <TrainingCopyPatch />
   </React.StrictMode>,
 )
