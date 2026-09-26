ac7e7896d60b64ad3c32e6ebe567af4922a9f7c4
diegoalphanogueira-commits <diegoalphanogueira@gmail.com>
2026-09-26 01:04:58 -0300

    Load desktop training section refinement

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './korax-landing-v6.css'
 import './pain-premium.css'
 import './frustration-premium.css'
 import './frustration-desktop-fix.css'
+import './training-desktop-fix.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
