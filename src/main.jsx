3e98f4156947d505073ca5b9653bbd057c713ec9
diegoalphanogueira-commits <diegoalphanogueira@gmail.com>
2026-09-25 21:58:14 -0300

    style: load premium IA comparison overrides

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './korax-landing-v5.css'
 import './v5plus-authority-footer.css'
 import './korax-landing-v6.css'
 import './pain-premium.css'
+import './frustration-premium.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
